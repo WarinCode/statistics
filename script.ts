919975becc6d4b23b5f8d84a9af58b27dfe98dd8
VarinCode <94356640+VarinCode@users.noreply.github.com>
2023-02-14 10:47:18 +0700

    Delete script.ts

--- script.ts
+++
@@ -1,8 +0,0 @@
-import { ValentinesDay } from "./main";
-
-(async function (love:boolean):Promise<void> {
-    let date:string = new Date().toISOString();
-    return await ValentinesDay(love)
-        .then((res) => console.log(`${date}: "${res}"`))
-        .catch((err) => console.error(err));
-})(true && true);
