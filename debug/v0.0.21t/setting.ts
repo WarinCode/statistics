7ed847546e0bfda8df750c56df5c4f8f98f254ac
VarinCode <94356640+VarinCode@users.noreply.github.com>
2023-06-08 22:00:08 +0700

    Add files via upload

---
+++ debug/v0.0.21t/setting.ts
@@ -0,0 +1,10 @@
+import { config } from "./main";
+
+// การตั้งค่าพื้นฐานของโปรแกรมสามารถเปลี่ยนค่า true , fasle ใน object นี้ได้เลย
+let object: config = {
+    displayText:true,
+    decimal: true,
+    usingConsole: false,
+};
+
+export { object };
