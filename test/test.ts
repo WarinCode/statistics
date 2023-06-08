016a2f1b8fbe46bf761dbc90fbf8aae9dfd31f41
VarinCode <94356640+VarinCode@users.noreply.github.com>
2023-06-08 22:03:41 +0700

    Update test.ts

--- test/test.ts
+++ test/test.ts
@@ -1,27 +1,46 @@
 import Statistics from "../main";
 import Extension from "../plugin/extensions";
+import figlet from 'figlet';
 
 let status = true;
+
+async function textFiglet(msg:string):Promise<void>{
+    console.log(
+        figlet.textSync(msg, {
+          font: "Univers",
+          horizontalLayout: "default",
+          verticalLayout: "default",
+          width: 160,
+          whitespaceBreak: true,
+        })
+      );
+}
+
 async function testProgram(): Promise<void> {
-    const arr = new Extension([], []).generateNumbers(10,'h');
-    const stat =  new Statistics(arr);
+    const arr:number[] = new Extension([], []).generateNumbers(10,'h');
+    const Stat:Statistics =  new Statistics(arr);
     try {
+        textFiglet('Start Program!');
         console.group('เริ่มทำการทดสอบโปรแกรม');
-        stat.showData();
-        console.log(stat.mean());
-        console.log(stat.median());
-        console.log(stat.mode());
-        console.log(stat.range());
-        console.log(stat.quartileDeviation());
-        console.log(stat.meanDeviation());
-        console.log(stat.standardDeviation());
+        Stat.showData();
+        console.log(Stat.mean());
+        console.log(Stat.median());
+        console.log(Stat.mode());
+        console.log(Stat.range());
+        console.log(Stat.quartileDeviation());
+        console.log(Stat.meanDeviation());
+        console.log(Stat.standardDeviation());
         console.groupEnd();
     } catch (err: unknown) {
         status = !status;
         throw err;
     }
 }
-testProgram().finally(() => {
-    if (status) console.log('ทดสอบโปรแกรมเสร็จสิ้นโปรแกรมพร้อมใช้งาน');
-    else if (status) console.error('ทดสอบโปรแกรมเสร็จสิ้นโปรแกรมยังไม่พร้อมใช้งาน');
-})
+testProgram()
+    .finally(() => {
+    if (status) {
+        textFiglet('Test the program is complete, the program is ready to use.');
+    } else {
+        textFiglet('Test the program is complete, the program is not available yet. please try again');
+    }
+});
